f6dda123c46f0c80a6469eb44963fd03c4ac5566
Palmeras3012 <palmerita12365@gmail.com>
2026-09-15 17:02:23 -0400

    Update bot connection details and server message

--- index.js
+++ index.js
@@ -2,28 +2,25 @@ const http = require('http');
 const mineflayer = require('mineflayer');
 const config = require('./config.json');
 
-// Servidor web para mantener Render y UptimeRobot vivos 24/7
 http.createServer((req, res) => {
-  res.write("Bot de Aternos activo 24/7");
+  res.write("Bot de Falix activo 24/7");
   res.end();
 }).listen(process.env.PORT || 3000);
 
 function createBot() {
-  console.log('Conectando bot a Aternos...');
+  console.log('Conectando bot a Falix...');
 
-const bot = mineflayer.createBot({
-  host: config.ip,
-  port: config.port,
-  username: config.username,
-  version: "1.21.9", // ⚠️ cámbialo por la versión real de tu server
-  auth: 'offline',    // quítalo si tu server NO es crackeado
-  hideErrors: false
-});
+  const bot = mineflayer.createBot({
+    host: config.ip,
+    port: config.port,
+    username: config.username,
+    version: "1.21.11",
+    auth: 'offline',
+    hideErrors: false
+  });
 
   bot.on('spawn', () => {
     console.log('¡ÉXITO: El bot ha entrado al servidor!');
-
-    // Auto-login para superar AuthMe
     setTimeout(() => {
       bot.chat('/register BotClave123 BotClave123');
       bot.chat('/login BotClave123');
